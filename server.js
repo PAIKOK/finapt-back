bd0a52feabb461abf443ac98d5e52ce888483591
PAIKOK <babujyothika003@gmail.com>
2025-07-08 19:35:19 +0530

    frontend on serverjs route api commit

--- server.js
+++ server.js
@@ -16,7 +16,11 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: 'https://finapt.netlify.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 app.use(express.json());
 
 // Register routes
